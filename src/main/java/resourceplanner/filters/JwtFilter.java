e009b014deec5af809058c2beb26e7a817413ac8
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-01-19 04:07:04 -0500

    Hotfix for custom servlet response for status code errors

--- src/main/java/resourceplanner/filters/JwtFilter.java
+++ src/main/java/resourceplanner/filters/JwtFilter.java
@@ -13,40 +13,44 @@ import org.springframework.web.filter.GenericFilterBean;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureException;
+import utilities.ExceptionCreator;
 
 /**
  * Created by jiaweizhang on 1/17/2016.
  */
 public class JwtFilter extends GenericFilterBean {
-
     @Override
     public void doFilter(final ServletRequest req,
                          final ServletResponse res,
                          final FilterChain chain) throws IOException, ServletException {
+        System.out.println("JwtFilter");
         final HttpServletRequest request = (HttpServletRequest) req;
-
         final String authHeader = request.getHeader("Authorization");
         if (authHeader == null) {
-            throw new ServletException("Missing Authorization header.");
+            res.setContentType("application/json;charset=UTF-8");
+            res.getWriter().write(ExceptionCreator.createJson("Missing Authorization header."));
+            return;
         }
         if (!authHeader.startsWith("Bearer ")) {
-            throw new ServletException("Invalid Authorization header.");
+            res.setContentType("application/json;charset=UTF-8");
+            res.getWriter().write(ExceptionCreator.createJson("Invalid Authorization header."));
+            return;
         }
-
-        final String token = authHeader.substring(7); // The part after "Bearer "
-
+        final String token = authHeader.substring(7);
         try {
             final Claims claims = Jwts.parser().setSigningKey("secretkey")
                     .parseClaimsJws(token).getBody();
-            /*if (claims.getExpiration().before(new Date())) {
-                throw new ServletException("Expired token");
-            }*/
             request.setAttribute("claims", claims);
         }
         catch (final SignatureException e) {
-            throw new ServletException("Invalid token.");
+            res.setContentType("application/json;charset=UTF-8");
+            res.getWriter().write(ExceptionCreator.createJson("Invalid token."));
+            return;
+        } catch (Exception f) {
+            res.setContentType("application/json;charset=UTF-8");
+            res.getWriter().write(ExceptionCreator.createJson("Invalid token."));
+            return;
         }
-
         chain.doFilter(req, res);
     }
 
---
+++ src/main/java/utilities/ExceptionCreator.java
@@ -0,0 +1,20 @@
+package utilities;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import responses.StandardResponse;
+
+/**
+ * Created by jiaweizhang on 1/19/2016.
+ */
+public class ExceptionCreator {
+    public static String createJson(String message) {
+        ObjectMapper mapper = new ObjectMapper();
+        StandardResponse response = new StandardResponse(true, message);
+        try {
+            String json = mapper.writeValueAsString(response);
+            return json;
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
