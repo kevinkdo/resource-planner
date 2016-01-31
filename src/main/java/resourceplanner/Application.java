df2baf74f713f1da524c946f7b65138249a7c323
Michael Han <michael.mj.han@gmail.com>
2016-01-31 04:34:53 +0000

    works on both http and https at same time now

--- src/main/java/resourceplanner/Application.java
+++ src/main/java/resourceplanner/Application.java
@@ -11,6 +11,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 import resourceplanner.filters.JwtFilter;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.apache.catalina.*;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+
+import java.io.IOException;
+import org.apache.coyote.http11.Http11NioProtocol;
+
+
 @EnableAutoConfiguration
 @ComponentScan
 @Configuration
@@ -23,6 +34,55 @@ public class Application {
         registrationBean.addUrlPatterns("/api/*");
 
         return registrationBean;
+    }
+/*
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+              SecurityConstraint securityConstraint = new SecurityConstraint();
+              securityConstraint.setUserConstraint("CONFIDENTIAL");
+              SecurityCollection collection = new SecurityCollection();
+              collection.addPattern("/*");
+              securityConstraint.addCollection(collection);
+              context.addConstraint(securityConstraint);
+            }
+          };
+        
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+      }
+      
+      private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        
+        return connector;
+      }*/
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        //try {
+            connector.setScheme("http");
+            connector.setSecure(false);
+            connector.setPort(8080);
+            return connector;
+        /*}
+        catch (IOException ex) {
+            throw new IllegalStateException("can't access keystore: [" + "keystore"
+                    + "] or truststore: [" + "keystore" + "]", ex);
+        }*/
     }
 
     public static void main(String[] args) {
