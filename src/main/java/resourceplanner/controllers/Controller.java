0fb415bf65b9fd56f49f4a9a6c14c0096df185d6
jiaweizhang <jiaweizhang95@gmail.com>
2016-02-01 23:51:21 -0500

    Added nullchecks to token so all tokens will be valid

--- src/main/java/resourceplanner/controllers/Controller.java
+++ src/main/java/resourceplanner/controllers/Controller.java
@@ -7,12 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 public class Controller {
 	public boolean isAdmin(HttpServletRequest request){
 		final Claims claims = (Claims) request.getAttribute("claims");
+		if (claims.get("permission") == null) {
+			return false;
+		}
         int permission = Integer.parseInt(claims.get("permission").toString());
         return permission == 1;
 	}
 
 	public int getRequesterID(HttpServletRequest request){
 		final Claims claims = (Claims) request.getAttribute("claims");
+		if (claims.get("user_id") == null) {
+			return 0; // TODO decide what to do
+		}
     	//Verify that the user_id in the requesrt == current user
     	int requesterID = Integer.parseInt(claims.get("user_id").toString());
     	return requesterID;
