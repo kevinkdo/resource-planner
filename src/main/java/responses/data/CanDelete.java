b04f50645eb07c780df9f63c5250c1fec3ef1be9
jiaweizhang <jiaweizhang95@gmail.com>
2016-02-03 14:51:56 -0500

    Added canDelete

---
+++ src/main/java/responses/data/CanDelete.java
@@ -0,0 +1,16 @@
+package responses.data;
+
+/**
+ * Created by jiaweizhang on 2/3/16.
+ */
+public class CanDelete {
+    private boolean canDelete;
+
+    public CanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public boolean getCanDelete() {
+        return canDelete;
+    }
+}
