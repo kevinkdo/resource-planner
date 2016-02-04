4c77e35a0028cef58dce787ff2f83e66ed759571
Michael Han <michael.mj.han@gmail.com>
2016-02-04 00:24:27 +0000

    updated test with post for Get Tag

--- src/test/test_TagTestCases.py
+++ src/test/test_TagTestCases.py
@@ -1,6 +1,6 @@
 import unittest
-import requests
 import json
+import requests
 
 class TestTagTestCases(unittest.TestCase):
   def setUp(self):
@@ -8,11 +8,12 @@ class TestTagTestCases(unittest.TestCase):
       self.headers = {'Accept': 'application/json', "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwidXNlcl9pZCI6MSwicGVybWlzc2lvbiI6MX0.r68KlS3szkDOUYvyGTf1HUG1nkF2U8WMM5u3AN0AFfI", "Content-Type": "application/json" }
       self.resetUrl = '/admin/init'
       self.tagUrl = 'api/tags'
-      requests.get(self.baseUrl + self.resetUrl, headers = self.headers)
+      r = requests.get(self.baseUrl + self.resetUrl, headers = self.headers)
+      print r.text
 
   def test_GetInitialTags(self):      
       r = requests.get(self.baseUrl + self.tagUrl, headers = self.headers)
-      decoded =  json.loads(r.text)
+      decoded =  r.json()#json.loads(r.text)
       assert decoded['is_error'] == False 
       assert decoded['data'] == {'tags': []}
       assert decoded['error_msg'] == 'Successfully retrieved  tags'
@@ -20,9 +21,11 @@ class TestTagTestCases(unittest.TestCase):
   def test_GetTagsAfterPosting(self):
       resource = {"name":"some resource", "description":"some resource description", "tags":["tag1","tag2"]}
       postUrl = 'api/resources'
-      r = requests.post(self.baseUrl + postUrl, data = resource, headers = self.headers)
-      decoded = json.loads(r.text)
-      print decoded
+      r = requests.post(self.baseUrl + postUrl, data = json.dumps(resource), headers = self.headers)
+      decoded = r.json()
+      assert decoded['is_error'] == False
+      assert decoded['data'] == {u'description': u'some resource description', u'tags': [u'tag1', u'tag2'], u'name': u'some resource', u'resource_id': 1}
+      assert decoded['error_msg'] == 'successful resource insert'
 
 if __name__ == '__main__':
     unittest.main()
