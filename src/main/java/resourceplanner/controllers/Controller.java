987658290c430a802d7beb0f9d4aba4b426fa8d8
jiaweizhang <jiaweizhang95@gmail.com>
2016-02-05 00:27:18 -0500

    Added Notfound exception

--- src/main/java/resourceplanner/controllers/Controller.java
+++ src/main/java/resourceplanner/controllers/Controller.java
@@ -1,8 +1,17 @@
 package resourceplanner.controllers;
 
 import io.jsonwebtoken.Claims;
+import org.postgresql.util.PSQLException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import responses.StandardResponse;
+import utilities.NotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class Controller {
 	public boolean isAdmin(HttpServletRequest request){
@@ -22,5 +31,33 @@ public class Controller {
     	//Verify that the user_id in the request == current user
     	int requesterID = Integer.parseInt(claims.get("user_id").toString());
     	return requesterID;
+	}
+
+	@ExceptionHandler(PSQLException.class)
+	@ResponseBody
+	public StandardResponse psqlException(Exception ex, HttpServletResponse response) {
+		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return new StandardResponse(true, "Database error");
+	}
+
+	@ExceptionHandler(BadSqlGrammarException.class)
+	@ResponseBody
+	public StandardResponse sqlException(Exception ex, HttpServletResponse response) {
+		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return new StandardResponse(true, "Database error");
+	}
+
+	@ExceptionHandler(NotFoundException.class)
+	@ResponseBody
+	public StandardResponse notFound(Exception ex, HttpServletResponse response) {
+		response.setStatus(HttpStatus.NOT_FOUND.value());
+		return new StandardResponse(true, "Page not found");
+	}
+
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	public StandardResponse exception(Exception ex, HttpServletResponse response) {
+		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return new StandardResponse(true, "Internal server error");
 	}
 }
---
+++ src/main/java/utilities/NotFoundException.java
@@ -0,0 +1,8 @@
+package utilities;
+
+/**
+ * Created by root on 2/5/16.
+ */
+public class NotFoundException extends Exception {
+    public NotFoundException () {}
+}
