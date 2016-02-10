84f16fa90a274c5e83267fcb29188257d2bf311c
jiaweizhang <jiaweizhang95@gmail.com>
2016-02-09 23:23:47 -0500

    Added pg_dump instructions for future reference

--- dbsetup.sh
+++ dbsetup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#python Evolution1Setup.py
+# Use API to populate
+# python Evolution1Setup.py
+
+# Direct psql populate
 psql -h localhost -d rp -U postgres -f ev1.sql
+
+# do this to dump the database to file
+# pg_dump rp -U postgres -h localhost -F c --file=ev1.sql
