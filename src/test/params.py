16e1f908447df8064acf0eab1ca7860ba018973d
Michael Han <michael.mj.han@gmail.com>
2016-02-12 19:08:21 +0000

    refactored tests

---
+++ src/test/params.py
@@ -0,0 +1,13 @@
+protocol = 'https' # change to http for non SSL
+host = '://localhost' # ://easywebapi.com
+baseUrl = protocol + host
+resetUrl = '/admin/init' 
+tagUrl = '/api/tags'
+resourcesUrl = '/api/resources'
+registerUrl = '/auth/register'
+loginUrl = '/auth/login'
+reserveUrl = '/api/reservations'
+resourceUrl = '/api/resources'
+userUrl = '/api/users'
+headers = {'Accept': 'application/json', "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwidXNlcl9pZCI6MSwicGVybWlzc2lvbiI6MX0.r68KlS3szkDOUYvyGTf1HUG1nkF2U8WMM5u3AN0AFfI", "Content-Type": "application/json" }
+
--- src/test/test_ResourcesTestCases.py
+++ src/test/test_ResourcesTestCases.py
@@ -1,18 +1,15 @@
 import unittest
 import requests
 import json
+import params
 
 class AuthorizationTestCases(unittest.TestCase):
   def setUp(self):
-      self.baseUrl = 'https://localhost:443/'
-      self.headers = {'Accept': 'application/json', "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwidXNlcl9pZCI6MSwicGVybWlzc2lvbiI6MX0.r68KlS3szkDOUYvyGTf1HUG1nkF2U8WMM5u3AN0AFfI", "Content-Type": "application/json" }
-      self.resourceUrl = 'api/resources'
-      self.resetUrl = '/admin/init'
-      r = requests.get(self.baseUrl + self.resetUrl, headers = self.headers, verify = False)
+      r = requests.get(params.baseUrl + params.resetUrl, headers = params.headers, verify = False)
 
   def test_CreateValidResourceNoTags(self):
       validResource = {'name':'some resource', 'description':'some resource description', 'tags':[]}
-      r = requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource), headers = self.headers, verify = False)
+      r = requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource), headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'description': u'some resource description', u'tags': [], u'name': u'some resource', u'resource_id': 1}
@@ -20,7 +17,7 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_CreateValidResourceWithTags(self):
       validResource = {'name':'some resource', 'description':'some resource description', 'tags':['tag1', 'tag2']}
-      r = requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource), headers = self.headers, verify = False)
+      r = requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource), headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'description': u'some resource description', u'tags': ['tag1', 'tag2'], u'name': u'some resource', u'resource_id': 1}
@@ -28,7 +25,7 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_CreateInvalidResourceNoName(self):
       validResource = {'name':'', 'description':'some resource description', 'tags':[]}
-      r = requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource), headers = self.headers, verify = False)
+      r = requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource), headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == True
       assert decoded['data'] == None
@@ -36,7 +33,7 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_CreateInvalidResourceNoDescription(self):
       validResource = {'name':'some resource', 'description':'', 'tags':[]}
-      r = requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource), headers = self.headers, verify = False)
+      r = requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource), headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'description': u'', u'tags': [], u'name': u'some resource', u'resource_id': 1}
@@ -44,15 +41,15 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_GetResourceWithValidID(self):
       validResource = {'name':'some resource', 'description':'some resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource), headers = self.headers, verify = False)
-      r = requests.get(self.baseUrl + self.resourceUrl + '/1', headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource), headers = params.headers, verify = False)
+      r = requests.get(params.baseUrl + params.resourceUrl + '/1', headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'description': u'some resource description', u'tags': ['tag1', 'tag2'], u'name': u'some resource', u'resource_id': 1}
       assert decoded['error_msg'] == 'Successfully retrieved resource'
 
   def test_GetResourceWithInvalidID(self):
-      r = requests.get(self.baseUrl + self.resourceUrl + '/1', headers = self.headers, verify = False)
+      r = requests.get(params.baseUrl + params.resourceUrl + '/1', headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == True
       assert decoded['data'] == None
@@ -62,11 +59,11 @@ class AuthorizationTestCases(unittest.TestCase):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
       validResource2 = {'name':'resource2', 'description':'resource description', 'tags':['tag2']}
       validResource3 = {'name':'resource3', 'description':'resource description', 'tags':['tag2', 'tag3']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource2), headers = self.headers, verify = False)
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource3), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource2), headers = params.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource3), headers = params.headers, verify = False)
       queryUrl = '/?required_tags=tag2,tag1&excluded_tags=tag3'
-      r = requests.get(self.baseUrl + self.resourceUrl + queryUrl, headers = self.headers, verify = False)
+      r = requests.get(params.baseUrl + params.resourceUrl + queryUrl, headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'resources': [{u'description': u'resource description', u'tags': [u'tag1', u'tag2'], u'name': u'resource1', u'resource_id': 1}]}
@@ -76,11 +73,11 @@ class AuthorizationTestCases(unittest.TestCase):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
       validResource2 = {'name':'resource2', 'description':'resource description', 'tags':['tag2']}
       validResource3 = {'name':'resource3', 'description':'resource description', 'tags':['tag2', 'tag3']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource2), headers = self.headers, verify = False)
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource3), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource2), headers = params.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource3), headers = params.headers, verify = False)
       queryUrl = '/?required_tags=tag4'
-      r = requests.get(self.baseUrl + self.resourceUrl + queryUrl, headers = self.headers, verify = False)
+      r = requests.get(params.baseUrl + params.resourceUrl + queryUrl, headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'resources': []}
@@ -88,10 +85,10 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_PutResourceWithValidIDAllFieldsUpdated(self):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
       update = {'name':'newname', 'description':'new description', 'tags':['tag3', 'tag4']}
       putUrl = '/1'
-      r = requests.put(self.baseUrl + self.resourceUrl + putUrl, data = json.dumps(update), headers = self.headers, verify = False)
+      r = requests.put(params.baseUrl + params.resourceUrl + putUrl, data = json.dumps(update), headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'description': u'new description', u'tags': [u'tag3', u'tag4'], u'name': u'newname', u'resource_id': 1}
@@ -99,10 +96,10 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_PutResourceWithValidIDNoFieldsUpdated(self):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
       update = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
       putUrl = '/1'
-      r = requests.put(self.baseUrl + self.resourceUrl + putUrl, data = json.dumps(update), headers = self.headers, verify = False)
+      r = requests.put(params.baseUrl + params.resourceUrl + putUrl, data = json.dumps(update), headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'description': u'resource description', u'tags': [u'tag1', u'tag2'], u'name': u'resource1', u'resource_id': 1}
@@ -110,10 +107,10 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_PutResourceWithInvalidID(self):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
       update = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
       putUrl = '/2'
-      r = requests.put(self.baseUrl + self.resourceUrl + putUrl, data = json.dumps(update), headers = self.headers, verify = False)
+      r = requests.put(params.baseUrl + params.resourceUrl + putUrl, data = json.dumps(update), headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == True
       assert decoded['data'] == None
@@ -121,9 +118,9 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_DeleteResourceWithValidID(self):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
       deleteUrl = '/1'
-      r = requests.delete(self.baseUrl + self.resourceUrl + deleteUrl, headers = self.headers, verify = False)
+      r = requests.delete(params.baseUrl + params.resourceUrl + deleteUrl, headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == None
@@ -131,9 +128,9 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_DeleteResourceWithInvalidID(self):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
       deleteUrl = '/2'
-      r = requests.delete(self.baseUrl + self.resourceUrl + deleteUrl, headers = self.headers, verify = False)
+      r = requests.delete(params.baseUrl + params.resourceUrl + deleteUrl, headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == True
       assert decoded['data'] == None
@@ -142,9 +139,9 @@ class AuthorizationTestCases(unittest.TestCase):
 
   def test_GetResourcesCanDeleteWithValidID(self):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
       canDeleteUrl = '/1/candelete'
-      r = requests.get(self.baseUrl + self.resourceUrl + canDeleteUrl, headers = self.headers, verify = False)
+      r = requests.get(params.baseUrl + params.resourceUrl + canDeleteUrl, headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'canDelete': True}
@@ -152,9 +149,9 @@ class AuthorizationTestCases(unittest.TestCase):
     
   def test_GetResourcesCanDeleteWithInvalidID(self):
       validResource1 = {'name':'resource1', 'description':'resource description', 'tags':['tag1', 'tag2']}
-      requests.post(self.baseUrl + self.resourceUrl, data = json.dumps(validResource1), headers = self.headers, verify = False)
+      requests.post(params.baseUrl + params.resourceUrl, data = json.dumps(validResource1), headers = params.headers, verify = False)
       canDeleteUrl = '/2/candelete'
-      r = requests.get(self.baseUrl + self.resourceUrl + canDeleteUrl, headers = self.headers, verify = False)
+      r = requests.get(params.baseUrl + params.resourceUrl + canDeleteUrl, headers = params.headers, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == False
       assert decoded['data'] == {u'canDelete': True}
@@ -164,7 +161,7 @@ class AuthorizationTestCases(unittest.TestCase):
   def test_GetResourcesCanDeleteWithInvalidUserHeaders(self):
       userHeaders = {'Accept': 'application/json', "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwidXNlcl9pZCI6MiwicGVybWlzc2lvbiI6MH0.5Br_NG0u_Kg2u11xyKqTGZAifyFHd19ca_HRRhu4j9Q", "Content-Type": "application/json" }
       canDeleteUrl = '/2/candelete'
-      r = requests.get(self.baseUrl + self.resourceUrl + canDeleteUrl, headers = userHeaders, verify = False)
+      r = requests.get(params.baseUrl + params.resourceUrl + canDeleteUrl, headers = userHeaders, verify = False)
       decoded = r.json()
       assert decoded['is_error'] == True
       #print decoded
