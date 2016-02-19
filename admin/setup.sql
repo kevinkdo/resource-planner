777b1c68db8daa11ec84d1c320945f1c6eeee63a
Davis Treybig <davis.treybig@gmail.com>
2016-02-19 13:04:39 -0500

    DB setup updated

--- admin/setup.sql
+++ admin/setup.sql
@@ -1,27 +1,33 @@
+DROP TABLE IF EXISTS groupmembers;
+DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS reservations;
 DROP TABLE IF EXISTS resourcetags;
 DROP TABLE IF EXISTS resources;
 DROP TABLE IF EXISTS users;
-
+DROP TABLE IF EXISTS groupResourcePermissions;
+DROP TABLE IF EXISTS userResourcePermissions;
 
 CREATE TABLE users (
-  user_id      SERIAL PRIMARY KEY  NOT NULL,
-  email        VARCHAR(255) UNIQUE NOT NULL,
-  passhash     VARCHAR(255)        NOT NULL,
-  username     VARCHAR(255) UNIQUE NOT NULL,
-  permission   INT                 NOT NULL,
-  should_email BOOLEAN             NOT NULL
+  user_id       SERIAL PRIMARY KEY  NOT NULL,
+  email         VARCHAR(255) UNIQUE NOT NULL,
+  passhash      VARCHAR(255)        NOT NULL,
+  username      VARCHAR(255) UNIQUE NOT NULL,
+  resource_p    BOOLEAN             NOT NULL,
+  reservation_p BOOLEAN             NOT NULL,
+  user_p        BOOLEAN             NOT NULL,
+  should_email  BOOLEAN             NOT NULL
 );
 
-INSERT INTO users (email, username, passhash, permission, should_email)
+INSERT INTO users (email, username, passhash, resource_p, reservation_p, user_p, should_email)
 VALUES ('admin@admin.com', 'admin',
         '1000:9816dd56235c68a566b1f50a1815ab96761ebf7ad33d84cd:5b209a5f9b1628fbd80cdffb0aa50b7ec58f07e93f9b18fc',
-        1, FALSE);
+        TRUE, TRUE, TRUE, FALSE);
+
 
 CREATE TABLE resources (
-  resource_id SERIAL PRIMARY KEY NOT NULL,
-  name        VARCHAR(255)       NOT NULL,
-  description VARCHAR(2000)
+  resource_id     SERIAL PRIMARY KEY NOT NULL,
+  name            VARCHAR(255)       NOT NULL,
+  description     VARCHAR(2000)
 );
 
 CREATE TABLE resourcetags (
@@ -37,4 +43,34 @@ CREATE TABLE reservations (
   end_time       TIMESTAMP,
   should_email   BOOLEAN            NOT NULL
 );
+
+CREATE TABLE groups (
+  group_id      SERIAL PRIMARY KEY NOT NULL,
+  group_name    VARCHAR(255)       NOT NULL,
+  resource_p    BOOLEAN            NOT NULL,
+  reservation_p BOOLEAN            NOT NULL,
+  user_p        BOOLEAN            NOT NULL
+);
+
+CREATE TABLE groupmembers (
+  group_id INT NOT NULL REFERENCES groups (group_id) ON DELETE CASCADE,
+  user_id  INT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+  UNIQUE (group_id, user_id)
+);
+
+CREATE TABLE groupResourcePermissions(
+  group_id INT NOT NULL REFERENCES groups (group_id) ON DELETE CASCADE,
+  resource_id INT NOT NULL REFERENCES resources (resource_id) ON DELETE CASCADE,
+  permission_level INT NOT NULL,
+  UNIQUE (group_id, resource_id)
+);
+
+CREATE TABLE userResourcePermissions(
+  user_id INT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+  resource_id INT NOT NULL REFERENCES resources (resource_id) ON DELETE CASCADE,
+  permission_level INT NOT NULL,
+  UNIQUE (user_id, resource_id)
+);
+
+
 
