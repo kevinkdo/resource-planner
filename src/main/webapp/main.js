4d990dd7670c497966eba03d7418e68445e92c5b
Kevin Do <kevin.kydat.do@gmail.com>
2016-02-21 16:03:43 -0500

    Remove admin console and set up routing to an empty group manager

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -71,8 +71,8 @@ const Router = React.createClass({
     switch (this.state.route) {
       case "login":
         return <Login setPstate={this.setState.bind(this)} pstate={this.state} />
-      case "admin_console":
-        return <AdminConsole setPstate={this.setState.bind(this)} pstate={this.state} />
+      case "group_manager":
+        return <GroupManager setPstate={this.setState.bind(this)} pstate={this.state} />
       case "settings":
         return <Settings setPstate={this.setState.bind(this)} pstate={this.state} />
       case "reservation_list":
@@ -153,7 +153,7 @@ const Navbar = React.createClass({
               <li className="dropdown">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-cog" aria-hidden="true"></span></a>
                 <ul className="dropdown-menu">
-                  <li><a href="#" onClick={() => this.props.setPstate({route: "admin_console"})}>Admin Console</a></li>
+                  <li><a href="#" onClick={() => this.props.setPstate({route: "group_manager"})}>Group Manager</a></li>
                   <li><a href="#" onClick={() => this.props.setPstate({route: "settings"})}>Settings</a></li>
                   <li role="separator" className="divider"></li>
                   <li><a href="#" onClick={this.logout}>Log Out</a></li>
@@ -167,42 +167,9 @@ const Navbar = React.createClass({
   }
 });
 
-const AdminConsole = React.createClass({
-  set(field, value) {
-    this.state[field] = value;
-    this.setState(this.state);
-  },
-
-  createUser(evt) {
-    evt.preventDefault();
-    var me = this;
-    this.setState({loading: true});
-    send_xhr("POST", "/api/users", localStorage.getItem("session"),
-      JSON.stringify({username:this.state.username, password:this.state.password, email: this.state.email, should_email: this.state.should_email}),
-      function(obj) {
-        me.props.setPstate({ route: "reservation_list" });
-      },
-      function(obj) {
-        me.setState({loading: false, error_msg: obj.error_msg});
-      }
-    );
-  },
-
-  cancel() {
-    this.props.setPstate({
-      route: "reservation_list"
-    });
-  },
-
+const GroupManager = React.createClass({
   getInitialState() {
-    return {
-      email: "",
-      username: "",
-      password: "",
-      should_email: false,
-      loading: false,
-      error_msg: ""
-    };
+    return {};
   },
 
   render() {
@@ -211,37 +178,7 @@ const AdminConsole = React.createClass({
         <Navbar setPstate={this.props.setPstate} pstate={this.props.pstate}/>
 
         <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-md-offset-3">
-              <form>
-                <legend>New user</legend>
-                {!this.state.error_msg ? <div></div> :
-                  <div className="alert alert-danger">
-                    <strong>{this.state.error_msg}</strong>
-                  </div>
-                }
-                <div className="form-group">
-                  <label htmlFor="user_creator_email">Email</label>
-                  <input type="email" className="form-control" id="user_creator_email" placeholder="Email" value={this.state.email} onChange={(evt)=>this.set("email", evt.target.value)}/>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="user_creator_username">Username</label>
-                  <input type="text" className="form-control" id="user_creator_username" placeholder="Username" value={this.state.username} onChange={(evt)=>this.set("username", evt.target.value)}/>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="user_creator_password">Password</label>
-                  <input type="password" className="form-control" id="user_creator_username" placeholder="Password" value={this.state.password} onChange={(evt)=>this.set("password", evt.target.value)}/>
-                </div>
-                <div className="checkbox">
-                  <label htmlFor="user_creator_should_email"><input type="checkbox" id="user_creator_should_email" checked={this.state.should_email} onChange={(evt)=>this.set("should_email", evt.target.checked)}/>Email reminders</label>
-                </div>
-                <div className="btn-toolbar">
-                  <button type="submit" className="btn btn-primary" onClick={this.createUser}>Create user</button>
-                  <button type="button" className="btn btn-default" onClick={this.cancel}>Cancel</button>
-                </div>
-              </form>
-            </div>
-          </div>
+          Hi Michael
         </div>
       </div>
     );
