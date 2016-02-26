8dc4e033fabc59658f2e247dd6aeef7d85669280
Michael Han <michael.mj.han@gmail.com>
2016-02-26 05:16:45 +0000

    change so that you can login with username instead of email

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1300,7 +1300,7 @@ const Login = React.createClass({
     evt.preventDefault();
     var me = this;
     send_xhr("POST", "/auth/login", "o",
-      JSON.stringify({email:this.state.email, password:this.state.password}),
+      JSON.stringify({username:this.state.username, password:this.state.password}),
       function(obj) {
         localStorage.setItem("session", obj.data.token);
         me.props.setPstate({ route: "reservation_list" });
@@ -1313,7 +1313,7 @@ const Login = React.createClass({
 
   getInitialState() {
     return {
-      email: "",
+      username: "",
       password: "",
       error_msg: ""
     }
@@ -1334,8 +1334,8 @@ const Login = React.createClass({
                   </div>
                 }
                 <div className="form-group">
-                  <label htmlFor="login_email">Email address</label>
-                  <input type="email" className="form-control" id="login_email" placeholder="Email" onChange={(evt)=>this.set("email", evt.target.value)} value={this.state.email}/>
+                  <label htmlFor="login_username">Username</label>
+                  <input type="text" className="form-control" id="login_username" placeholder="Username" onChange={(evt)=>this.set("username", evt.target.value)} value={this.state.username}/>
                 </div>
                 <div className="form-group">
                   <label htmlFor="login_password">Password</label>
