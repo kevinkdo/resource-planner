c150780bfeedadf291929f69de640d6383771b12
Kevin Do <kevin.kydat.do@gmail.com>
2016-02-28 13:36:11 -0500

    Update setupvm.sh for frontend build tool errors due to moving to admin folder

--- admin/setupvm.sh
+++ admin/setupvm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# run 'chmod 755 setup' prior to being able to run script
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 sudo apt-get update
@@ -20,5 +19,7 @@ sudo apt-get install gradle
 sudo apt-get install nodejs
 sudo apt-get install npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+cd ..
 sudo npm install --global babel-cli
 sudo npm install babel-preset-react
+cd admin
