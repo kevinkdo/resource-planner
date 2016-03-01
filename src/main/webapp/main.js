c2ca8d6cd03e6a0d9d3ce0dce40e5bc37a3d9182
Michael Han <michael.mj.han@gmail.com>
2016-03-01 05:44:28 +0000

    previous users not showing up in group editor fixed

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -309,6 +309,8 @@ const GroupEditor = React.createClass({
   editGroup(evt) {
     var me = this;
     this.setState({sending: true});
+
+    console.log(this.state.user_ids)
     send_xhr("PUT", "/api/groups/" + this.props.id, localStorage.getItem("session"),
       JSON.stringify({group_name: this.state.group_name, user_ids: this.state.user_ids.filter(x => x.length > 0)}),
       function(obj) {
@@ -349,7 +351,11 @@ const GroupEditor = React.createClass({
     var me = this;
     send_xhr("GET", "/api/groups/" + this.props.id, localStorage.getItem("session"), null,
       function(obj) {
+        //console.log(obj.data.user_ids)
         obj.data.user_ids.push("");
+        for (var i = 0; i < obj.data.user_ids.length; i++) {
+          obj.data.user_ids[i] = obj.data.user_ids[i].toString();
+        }
         me.setState({
           group_name: obj.data.group_name,
           user_ids: obj.data.user_ids,
