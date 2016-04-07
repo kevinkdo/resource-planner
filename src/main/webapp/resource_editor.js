208082acc77cc0492dd7f63c2633d04ecf1836c5
Michael Han <michael.mj.han@gmail.com>
2016-04-06 23:34:24 -0400

    updated to have parent id and shared count in get requests

--- src/main/webapp/resource_editor.js
+++ src/main/webapp/resource_editor.js
@@ -4,7 +4,7 @@ const ResourceEditor = React.createClass({
     var me = this;
     this.setState({sending: true});
     send_xhr("PUT", "/api/resources/" + this.props.pstate.view_id, localStorage.getItem("session"),
-      JSON.stringify({restricted: this.state.restricted, name:this.state.name, description:this.state.description || "", tags: this.state.tags.filter(x => x.length > 0)}),
+      JSON.stringify({restricted: this.state.restricted, name:this.state.name, description:this.state.description || "", tags: this.state.tags.filter(x => x.length > 0), parent_id: this.state.parent_id, shared_count: this.state.shared_count}),
       function(obj) {
         me.props.setPstate({ route: "resource_list", is_error: false, error_msg: "Successfully edited resource!" });
       },
