48f4fa1be92d743958d211c58d38cac5b30c17da
Michael Han <michael.mj.han@gmail.com>
2016-04-06 23:33:43 -0400

    updated frontend build script to not use babel

--- admin/frontend_build_once.sh
+++ admin/frontend_build_once.sh
@@ -2,5 +2,4 @@
 # Watches main.js and automatically rebuilds main-build.js
 cd ../src/main/webapp
 
-cat admin_console.js group_editor.js group_manager.js list_input.js loader.js login.js navbar.js permissions_manager.js reservation_creator.js reservation_editor.js reservation_list.js resource_creator.js resource_editor.js resource_list.js router.js settings.js main.js > main-concat.js
-babel --presets react main-concat.js -o main-build.js
+cat admin_console.js group_editor.js group_manager.js list_input.js loader.js login.js navbar.js permissions_manager.js reservation_creator.js reservation_editor.js reservation_list.js resource_creator.js resource_editor.js resource_list.js router.js settings.js main.js > main-build.js
