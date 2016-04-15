d6fceacf6d2a78777ba4d74151093f63efd9733b
Kevin Do <kevin.kydat.do@gmail.com>
2016-04-14 21:24:13 -0400

    Make selecting nodes possible again

--- src/main/webapp/resource_tree.js
+++ src/main/webapp/resource_tree.js
@@ -21,16 +21,20 @@ const ResourceTree = React.createClass({
     };
   },
 
-  setSelectedId(new_id) {
-    this.setState({selected_id: new_id, selected_link: {source_id: 0, target_id: 0}});
-  },
-
   setSelectedLink(new_link) {
     this.setState({selected_id: 0, selected_link: new_link})
   },
 
-  clearClick() {
-    this.setState({selected_id: 0, selected_link: {source_id: 0, target_id: 0}});
+  clickHandler() {
+    if (this.state.sourceId == 0) {
+      this.setState({selected_id: 0, selected_link: {source_id: 0, target_id: 0}});
+    } else {
+      // TODO Fire a PUT request here
+      this.setState({
+        sourceId: 0,
+        targetId: 0
+      });
+    }
   },
 
   setTargetId(targetId) {
@@ -45,7 +49,9 @@ const ResourceTree = React.createClass({
       xOffset: dragState.xOffset,
       yOffset: dragState.yOffset,
       clientX: dragState.clientX,
-      clientY: dragState.clientY
+      clientY: dragState.clientY,
+      selected_id: dragState.sourceId,
+      selected_link: {source_id: 0, target_id: 0}
     });
   },
 
@@ -53,13 +59,6 @@ const ResourceTree = React.createClass({
     this.setState({
       clientX: evt.clientX,
       clientY: evt.clientY
-    });
-  },
-
-  handleMouseUp: function() {
-    this.setState({
-      sourceId: 0,
-      targetId: 0
     });
   },
 
@@ -129,7 +128,7 @@ const ResourceTree = React.createClass({
       if (node.ignore) {
         return null;
       }
-      return <TreeNode key={node.id} x={node.x} y={node.y} name={node.name} resource_id={node.resource_id} setTargetId={me.setTargetId} sourceId={me.state.sourceId} targetId={me.state.targetId} startDrag={me.startDrag} subscript={node.subscript} restricted={node.restricted} refresh={me.refresh} deleteNode={me.deleteNode} setPstate={me.props.setPstate} setSelectedId={me.setSelectedId} is_selected={me.state.selected_id == node.resource_id}/>;
+      return <TreeNode key={node.id} x={node.x} y={node.y} name={node.name} resource_id={node.resource_id} setTargetId={me.setTargetId} sourceId={me.state.sourceId} targetId={me.state.targetId} startDrag={me.startDrag} subscript={node.subscript} restricted={node.restricted} refresh={me.refresh} deleteNode={me.deleteNode} setPstate={me.props.setPstate} is_selected={me.state.selected_id == node.resource_id}/>;
     });
 
     var renderedLinks = links.map(function(link) {
@@ -141,7 +140,7 @@ const ResourceTree = React.createClass({
 
     var helpText = <text className="helpText" x={0} y={0}>Click on a node to access node options. Click 'edit' to go to the resource edit page for that resource. Click the 'X' to delete the resource and make it's children children of the deleted node's parent. Click on a link to access link options. Click the 'X' to make the node and it's children it's own tree with the child node of the link becoming the new root. Click the clear button to unselect any selected nodes or links.</text>;
 
-    var svg = <svg id="mysvg" width={width} height={height} onClick={me.clearClick} onMouseMove={me.state.sourceId != 0 ? this.handleMouseMove : null} onMouseUp={me.state.sourceId != 0 ? this.handleMouseUp : null}>{renderedNodes}{renderedLinks}</svg>;
+    var svg = <svg id="mysvg" width={width} height={height} onClick={me.clickHandler} onMouseMove={me.state.sourceId != 0 ? this.handleMouseMove : null}>{renderedNodes}{renderedLinks}</svg>;
 
     return <div>{helpText}<br/>{svg}</div>;
   }
--- src/main/webapp/tree_node.js
+++ src/main/webapp/tree_node.js
@@ -27,11 +27,6 @@ const TreeNode = React.createClass({
     this.props.refresh();
   },
 
-  onNodeClick(evt) {
-    this.props.setSelectedId(this.props.resource_id);
-    evt.stopPropagation();
-  },
-
   handleMouseDown(evt) {
     this.props.startDrag({
       sourceId: this.props.resource_id,
@@ -54,9 +49,9 @@ const TreeNode = React.createClass({
     var dragging = this.props.sourceId != 0 ? true : false;
     var me_is_dragging = this.props.sourceId == this.props.resource_id;
 
-    var marker = <circle r="8" fill={this.getFill()} cx={this.props.x+8} cy={this.props.y+8} onClick={this.onNodeClick} onMouseDown={this.handleMouseDown}/>;
+    var marker = <circle r="8" fill={this.getFill()} cx={this.props.x+8} cy={this.props.y+8} onMouseDown={this.handleMouseDown} onMouseUp={this.handleMouseUp}/>;
     var circle = <circle className={dragging && !me_is_dragging ? "ghostCircle show" : "ghostCircle noshow"} r="30" cx={this.props.x + 8} cy={this.props.y + 8} opacity="0.4" fill={this.getCircleFill()} onMouseOver={this.handleMouseOver} onMouseOut={this.setBlueFill} onMouseOut={this.handleMouseOut} />;
-    var text = <text className="nodelabel" x={this.props.x + 20} y={this.props.y + 13}>{this.props.name}</text>;
+    var text = <text className="nodelabel nopointer" x={this.props.x + 20} y={this.props.y + 13}>{this.props.name}</text>;
     var deleteButton = <text className="deleteButton" x={this.props.x - 15} y={this.props.y + 13} onClick={this.handleDeleteClick}>{this.props.is_selected ? "X" : ""}</text>;
     var editButton = <text className="editButton" x={this.props.x - 45} y={this.props.y + 13} onClick={this.handleEditClick}>{this.props.is_selected ? "edit" : ""}</text>;
     return <g className={me_is_dragging ? "draggable nopointer" : "draggable yespointer"}>{marker}{circle}{deleteButton}{editButton}{text}</g>;
